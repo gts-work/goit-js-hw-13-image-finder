fa281c7306c0394eca40c5c9dd9298f2e8a866cb
Grygorii Tsaryk <artgrem@gmail.com>
2021-05-28 01:33:54 +0300

    fixed clear content after clear search in input

--- src/index.js
+++ src/index.js
@@ -1,66 +1,62 @@
-import "./sass/main.scss";
-import PixabayApiService from "./js/pixabayApi";
-import photosListTpl from "./templates/photos.hbs";
-import {
-    appendItemCountryMarkup,
-    cleanContent,
-    createPhotoModal,
-} from "./js/markups";
-import { refs } from "./js/refs";
-import { photoFetch } from "./js/fetchApi";
+import './sass/main.scss';
+import PixabayApiService from './js/pixabayApi';
+import photosListTpl from './templates/photos.hbs';
+import { appendItemCountryMarkup, cleanContent, createPhotoModal } from './js/markups';
+import { refs } from './js/refs';
+import { photoFetch } from './js/fetchApi';
 
-var debounce = require("lodash.debounce");
+var debounce = require('lodash.debounce');
 
 const pixabayApiService = new PixabayApiService();
 
 const inputButton = refs.inputBtn;
-inputButton.addEventListener("input", debounce(onInputSearch, 800));
+inputButton.addEventListener('input', debounce(onInputSearch, 800));
 
 const photoCard = refs.photoCard;
-photoCard.addEventListener("click", onClickPhotoCard);
+photoCard.addEventListener('click', onClickPhotoCard);
 
 // LOAD MORE
-const observer = new IntersectionObserver((entries) => {
-    const firstEntry = entries[0];
+const observer = new IntersectionObserver(entries => {
+  const firstEntry = entries[0];
 
-    if (firstEntry.isIntersecting) {
-        if (inputButton.value) {
-            pixabayApiService.page += 1;
+  if (firstEntry.isIntersecting) {
+    if (inputButton.value) {
+      pixabayApiService.page += 1;
 
-            const infoMsg =
-                "No more images. This is all that was found on your request";
-            const errMsg = "Invalid query.";
-            photoFetch(pixabayApiService, infoMsg, errMsg);
-        }
+      const infoMsg = 'No more images. This is all that was found on your request';
+      const errMsg = 'Invalid query.';
+      photoFetch(pixabayApiService, infoMsg, errMsg);
     }
+  }
 });
 
 const scrollArea = refs.scrollArea;
 observer.observe(scrollArea);
-console.log("observer 2: ", observer);
+console.log('observer 2: ', observer);
 // END LOAD MORE
 
 function onInputSearch(e) {
-    const photoQuery = e.target.value.trim();
-    console.log("onInputSearch ~ photoQuery: ", photoQuery);
+  const photoQuery = e.target.value.trim();
+  console.log('onInputSearch ~ photoQuery: ', photoQuery);
 
-    if (photoQuery) {
-        pixabayApiService.page = 1;
-        cleanContent(refs.container);
+  if (photoQuery) {
+    pixabayApiService.page = 1;
+    cleanContent(refs.container);
 
-        pixabayApiService.photo = photoQuery;
-        const infoMsg = "No more images";
-        const errMsg = "Invalid query.";
-        photoFetch(pixabayApiService, infoMsg, errMsg);
-    } else {
-        appendItemCountryMarkup(refs.container, photosListTpl, "");
-    }
+    pixabayApiService.photo = photoQuery;
+    const infoMsg = 'No more images';
+    const errMsg = 'Invalid query.';
+    photoFetch(pixabayApiService, infoMsg, errMsg);
+  } else {
+    console.log('onInputSearch ~ photoQuery 2: ', photoQuery);
+    appendItemCountryMarkup(refs.container, photosListTpl, '');
+  }
 }
 
 function onClickPhotoCard(e) {
-    const photoEl = e.target;
-    const largeImage = photoEl.dataset.largeImage;
-    const photoAlt = photoEl.getAttribute("alt");
+  const photoEl = e.target;
+  const largeImage = photoEl.dataset.largeImage;
+  const photoAlt = photoEl.getAttribute('alt');
 
-    createPhotoModal(largeImage, photoAlt);
+  createPhotoModal(largeImage, photoAlt);
 }
--- src/js/markups.js
+++ src/js/markups.js
@@ -1,18 +1,21 @@
-import * as basicLightbox from "basiclightbox";
+import * as basicLightbox from 'basiclightbox';
 
 export function appendItemCountryMarkup(ref, tpl, photos) {
-    // ref.innerHTML = "";
-    ref.insertAdjacentHTML("beforeend", tpl(photos));
+  if (photos) {
+    ref.insertAdjacentHTML('beforeend', tpl(photos));
+  } else {
+    ref.innerHTML = '';
+  }
 }
 
 export function cleanContent(ref) {
-    ref.innerHTML = "";
+  ref.innerHTML = '';
 }
 
 export function createPhotoModal(img, alt) {
-    const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(`
         <img src="${img}" alt="${alt}">
     `);
 
-    instance.show();
+  instance.show();
 }
